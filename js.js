31d3d4a3195e744b5c875ef12c63957e978c7727
Nick Kinlen <nick_kinlen@hotmail.com>
2017-06-16 00:14:21 -0400

    Backend finished

--- js.js
+++ js.js
@@ -1,30 +1,50 @@
 // Won't work on codepen unless codepen's address is HTTP, not HTTPS
 
 var API_KEY = "4334fcbdc100f11d073ef2fc6017110f";
+var cel = false;
+var wd;
+
+function displayTemp(fTemp, c){
+  if(c) return Math.round((fTemp - 32) * (5/9)) + " C";
+  return fTemp + " F";
+}
+
+function render(wd, cel){
+  var currentLocation = wd.name;
+  var currentWeather = wd.weather[0].description;
+  var currentTemp = displayTemp(wd.main.temp, cel);
+  var high = displayTemp(wd.main.temp_max, cel);
+  var low = displayTemp(wd.main.temp_min, cel);
+  var icon = wd.weather[0].icon;
+
+  $('#currentLocation').html(currentLocation);
+  $('#currentTemp').html(currentTemp + '&#8457');
+  $('#currentWeather').html(currentWeather);
+  $('#high-low').html(high + '/' + low);
+
+  var iconSrc = "http://openweathermap.org/img/w/" + icon + ".png";
+  $('#currentTemp').prepend('<img src="' + iconSrc + '">');
+}
 
 $(function(){
 
   var loc;
 
   $.getJSON('https://ipinfo.io', function(d){
-    console.log("Assigning the data")
     loc = d.loc.split(",");
     console.log(loc);
 
 
   $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=imperial&lat='
-   + loc[0] + '&lon=' + loc[1] + '&APPID=' + API_KEY, function(wd){
-      console.log("got the data,", wd);
-          var currentLocation = wd.name;
-          var currentWeather = wd.weather[0].description;
-          var currentTemp = wd.main.temp;
-          var high = wd.main.temp_max;
-          var low = wd.main.temp_min;
+   + loc[0] + '&lon=' + loc[1] + '&APPID=' + API_KEY, function(apiData){
+     wd = apiData;
 
-          $('#currentLocation').html(currentLocation);
-          $('#currentTemp').html(currentTemp + '&#8457');
-          $('#currentWeather').html(currentWeather);
+     render(apiData, cel);
 
+     $('#toggle').click(function(){
+          cel = !cel;
+          render(wd, cel);
+        })
 
       })
 
